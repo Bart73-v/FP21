945af75a016e7a3455cd6a410b15a3c5ff26416a
Bart73-v <bart.veldman@live.nl>
2021-09-17 11:03:44 +0200

    2.5 done

--- week 2/Char.hs
+++ week 2/Char.hs
@@ -13,9 +13,13 @@ reverseCase []      = []
 reverseCase (x:xs)  = if isUpper x  then toLower x : reverseCase xs 
                                     else toUpper x : reverseCase xs
 
---shift :: Int -> Char -> Char
+shift :: Int -> Char -> Char
+shift n c = if ord c < 91 && ord c > 64 then chr ((n + (ord c-65)) `mod` 26 + 65) else c
 
---caesar :: Int -> String -> String
+caesar :: Int -> String -> String
+caesar n = map (shift n . toUpper)
 
 msg :: String
 msg = "ADMNO D HPNO NKMDIFGZ TJP RDOC AVDMT YPNO"
+
+fairyDust = caesar 5 msg
