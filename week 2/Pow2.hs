c6cb7848bd4fe471cd15b88c6738b4db43faca84
Bart73-v <bart.veldman@live.nl>
2021-09-17 10:26:59 +0200

    ex3 done

--- week 2/Pow2.hs
+++ week 2/Pow2.hs
@@ -4,9 +4,8 @@ pow2 :: (Ord n, Num n, Num a) => n -> a
 pow2 0 = 1
 pow2 n = 2 * pow2 (n-1)
 
-
 -- Maximum n:
 -- Integer  -> Infinite
 -- Int      -> 2^29-1
--- Float    ->
--- Double   ->
+-- Float    -> We can't find the bound of floats
+-- Double   -> Infinite
