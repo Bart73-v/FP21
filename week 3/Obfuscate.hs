f7fe41132073e111418c4a8df6e252e641c4ba8b
Bart73-v <bart.veldman@live.nl>
2021-09-24 12:10:44 +0200

    3.8 almost done

--- week 3/Obfuscate.hs
+++ week 3/Obfuscate.hs
@@ -1,11 +1,13 @@
 module Obfuscate where
 
 import Data.Char
+import Data.List
 
 --Daan Eijkman
 --Bart Veldman
 
---cambridge :: String -> String
+cambridge :: String -> String
+cambridge xs = unwords (map jumble (words xs))
 
 meme :: String
 meme = "According to research at Cambridge university, it doesn't matter\
@@ -13,3 +15,11 @@ meme = "According to research at Cambridge university, it doesn't matter\
        \ that the first and last letters are at the right place. The rest can\
        \ be a total mess and you can still read it without a problem. This is\
        \ because we do not read every letter by it self but the word as a wohle."
+
+jumble (x:xs)
+       | xs == []  = [x]
+       | otherwise = [x] ++ (jumble' (tail (reverse xs))) ++ [(head (reverse xs))]
+
+jumble' []           = []
+jumble' [x]          = [x]
+jumble' (x1:x2:xs)     = permutations (x1:x2:xs) !! 0
