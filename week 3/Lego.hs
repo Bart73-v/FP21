9166826fa2ebd81720308faa721b1457ebf5f2a1
Bart73-v <bart.veldman@live.nl>
2021-09-24 11:37:21 +0200

    3.7 done

--- week 3/Lego.hs
+++ week 3/Lego.hs
@@ -12,5 +12,5 @@ removeAt n xs = [ x | (i,x) <- zip [1..] xs, i /= n]
 sortWithPos :: (Ord a) => [a] -> [(a,Int)]
 sortWithPos xs = sortOn fst (zip xs [0..])
 
-
---sortedPos :: (Ord a) => [a] -> [(a,Int)]
+sortedPos :: (Ord a) => [a] -> [(a,Int)]
+sortedPos xs = [(b,c) | ((a,b),c) <- (sortOn (fst . fst) (zip (sortOn snd (zip [0..] xs)) [0..]))]
