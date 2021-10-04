8e1552a4e73ee248383e33be5ae33068dfaa4aa0
Daan <daan@eijkman.nl>
2021-10-04 15:35:22 +0200

    4.6 done

--- week 4/AST.hs
+++ week 4/AST.hs
@@ -1,11 +1,16 @@
 module AST where
+import Data.Maybe
 
---start with either:
---  data Expr = Lit Integer | Add Expr Expr | Mul Expr Expr | ...
---or:
---  data Expr = Lit Integer | Expr :+: Expr | Expr :*: Expr | ...
---  infixl 6 :+:
---  infixl 7 :*:
+data Expr = Lit Integer | Add Expr Expr | Sub Expr Expr | Mul Expr Expr | Div Expr Expr | VarX
 
-
---eval :: (Fractional a, Eq a) => Expr -> a -> Maybe a
+eval :: (Fractional a, Eq a) => Expr -> a -> Maybe a
+eval expr b =
+           if parse expr b == parse (Div (Lit 1) (Lit 0)) b
+              then Nothing
+           else Just (parse expr b)
+                where parse (Add e1 e2) b = (parse e1 b) + (parse e2 b)
+                      parse (Sub e1 e2) b = (parse e1 b) - (parse e2 b)
+		      parse (Mul e1 e2) b = (parse e1 b) * (parse e2 b)
+		      parse (Div e1 e2) b = (parse e1 b) / (parse e2 b)
+		      parse (Lit i) b     = realToFrac i
+		      parse VarX b        = b
