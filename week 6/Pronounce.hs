54a49f5f52b026ffacb76465b24d76945e389f17
Daan <daan@eijkman.nl>
2021-10-13 14:44:00 +0200

    6.1

--- week 6/Pronounce.hs
+++ week 6/Pronounce.hs
@@ -11,3 +11,24 @@ instance Pronounceable Char where
 
 instance Pronounceable a => Pronounceable [a] where
   pronounce xs = "a list containing " ++ (intercalate ", " $ map pronounce xs)
+  
+instance Pronounceable Integer where
+  pronounce = say . toInteger
+
+instance Pronounceable Int where
+  pronounce = say . toInteger
+  
+-- A little bit hardcoded
+-- The idea: truncate i ++ " point " ++ round((i - truncate(i))*10)
+instance Pronounceable Double where
+  pronounce i = pronounce(truncate(i) :: Integer) ++  " point " ++ rounding(i)
+          where rounding :: Double -> String
+                rounding i = pronounce(round((i - (fromIntegral . truncate) i)*10) :: Integer)
+
+instance (Pronounceable a, Pronounceable b) => Pronounceable (a, b) where
+  pronounce (x, y) = "A tuple containing" ++ pronounce x ++ " and " ++ pronounce y
+ 
+data FuncWithArg a b = (a -> b) :$: a
+ 
+instance (Pronounceable a, Pronounceable b) => Pronounceable (FuncWithArg a b) where
+  pronounce (f :$: x) = "A function from " ++ pronounce x ++ " to " ++ pronounce (f x)
---
+++ week 6/Say.hs
@@ -0,0 +1,44 @@
+--Daan Eijkman
+--Bart Veldman
+
+module Say where
+
+say :: Integer -> String
+say n
+  | n > 999   = say (n `div` 1000) ++ " thousand" ++ spaces n 1000 ++ say (n `mod` 1000)
+  | n > 99    = say (n `div` 100) ++ " hundred" ++ spaces n 100 ++ say (n `mod` 100)
+  | n > 19    = decimals (n `div` 10) ++ spaces n 10 ++ say (n `mod` 10)
+  | n == 19   = "nineteen"
+  | n == 18   = "eighteen"
+  | n == 17   = "seventeen"
+  | n == 16   = "sixteen"
+  | n == 15   = "fifteen"
+  | n == 14   = "fourteen"
+  | n == 13   = "thirteen"
+  | n == 12   = "twelve"
+  | n == 11   = "eleven"
+  | n == 10   = "ten"
+  | n == 9    = "nine"
+  | n == 8    = "eight"
+  | n == 7    = "seven"
+  | n == 6    = "six"
+  | n == 5    = "five"
+  | n == 4    = "four"
+  | n == 3    = "three"
+  | n == 2    = "two"
+  | n == 1    = "one"
+  | otherwise = ""
+  where spaces n 100 = if n `mod` 100 == 0 then "" else " and "
+        spaces n m   = if   n `mod` m == 0 then "" else " "
+
+decimals :: Integer -> String 
+decimals 2 = "twenty"
+decimals 3 = "thirty"
+decimals 4 = "forty"
+decimals 5 = "fifty"
+decimals 6 = "sixty"
+decimals 7 = "seventy"
+decimals 8 = "eighty"
+decimals 9 = "ninety"
+decimals _ = ""
+
