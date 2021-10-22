4d71134bdfd2f06c6b2e94e41bf107868c3bf4ee
Daan <daan@eijkman.nl>
2021-10-22 11:57:20 +0200

    Empty.hs

--- week 7/Empty.hs
+++ week 7/Empty.hs
@@ -1,13 +1,21 @@
 module Empty where
 
 {- which ones of these functions are the improper way to test for [] ? -}
-
+-- isEmpty0 :: Foldable t => t a -> Bool
+-- isEmpty0 fails for an infinite list of intsand takes very long for many ints in the list.
 isEmpty0 list = length list == 0
 
+-- isEmpty1 :: Eq a => [a] -> Bool
+-- isEmpty1 doesn't work for a list filled with functions
 isEmpty1 list = list == []
 
+-- isEmpty2 :: Foldable t => t a -> Bool
+-- Works great
+-- This one is prefered because it uses a haskell prelude function
 isEmpty2 list = null list
 
+-- isEmpty3 :: [a] -> Bool
+-- Works great as wellx
 isEmpty3 []   = True
 isEmpty3 _    = False
 
